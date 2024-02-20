fc4ed5444967792e2271f43be2cc7b9a67cfd70e
Uzair-Janjua <i200694@nu.edu.pk>
2024-02-20 11:26:20 +0500

    Project Scaffodling and GithubAction Workflows

---
+++ main.py
@@ -0,0 +1,22 @@
+class mlops:
+
+	def __init__(self, totalStudents):
+		self.totalStudents = totalStudents
+
+	def getTotalStudents(self):
+		return self.totalStudents
+
+	def AddStudents(self):
+		self.totalStudents = self.totalStudents + 1
+
+	def removeStudents(self):
+		self.totalStudents = self.totalStudents - 1
+
+	def getClassName(self):
+		return "Machine Learning Operations CS-B"
+
+mlops_class = mlops(5)
+mlops_class.AddStudents()
+mlops_class.removeStudents()
+print(mlops_class.getTotalStudents())
+print(mlops_class.getClassName())
